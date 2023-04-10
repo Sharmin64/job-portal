f1d65af6657f26d1e7e236268ce5fe9660b8b31a
Sharmin64 <lizaeatsbanana@gmail.com>
2023-04-11 00:42:12 +0600

    style in home page changed

--- src/components/Demand/Demand.jsx
+++ src/components/Demand/Demand.jsx
@@ -4,7 +4,8 @@ import logo from './hireman.jpg'
 
 const Demand = () => {
   return (
-    <div className='flex flex-grid flex-col-reverse lg:flex-row grid-cols-2 mx-auto items-center justify-around'>
+    <>
+        <div className='flex flex-grid flex-col-reverse lg:flex-row grid-cols-2 mx-auto items-center justify-around'>
       <div className='my-3'>
         <h2 className='text-5xl font-bold mb-4'>One Step <br />Ahead to Your <br />  <span className='text-[#6897bb]'>Favourite Job</span></h2>
         <p className='pb-4'>Explore thousands of job opportunities <br/> with all the information you need. Its your future. <br/> Come find it. Manage all your job application  <br/>from start to finish.</p>
@@ -13,9 +14,14 @@ const Demand = () => {
         </Link>
       </div>
       <div>
-        <img className='object-cover mt-5 px-8 overflow-hidden' src={logo} alt="jobman" />
+        <img className='object-cover  mt-5 px-8 overflow-hidden' src={logo} alt="jobman" />
       </div>
-    </div>
+      </div>
+      <div className='mt-12 '>
+        <h2 className='text-5xl'>Petition Category List</h2>
+        <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+      </div>
+    </>
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,55 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
+  
     <nav className='flex flex-col lg:flex-row grid-cols-3 justify-around bg-[#f5f5f5]  items-center mx-auto'>
       <div>
         <h2 className='text-5xl font-serif'>JobWorld </h2>
       </div>
-      <div className='space-x-5 flex flex-col lg:flex-row p-2'>
+      <div className='space-x-5 flex flex-col lg:flex-row p-2 items-center'>
        
-        <Link to="/"><p>Home</p></Link>
-        <Link to="/statistics"><p>Statistics</p></Link>
-        <Link to="/applied jobs"><p>Applied Jobs</p></Link>
-        <Link to="/blog"><p>Blog</p></Link>
+      <NavLink
+              to='/'
+              aria-label='Home'
+              title='Home'
+              className={({ isActive }) => (isActive ? 'active' : 'font-medium tracking-wide text-gray-500  transition-colors duration-200')}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to='/statistics'
+              aria-label='statistics'
+              title='statistics'
+              className={({ isActive }) => (isActive ? 'active' : 'font-medium tracking-wide text-gray-500  transition-colors duration-200')}
+            >
+              Statistics
+            </NavLink>
+            <NavLink
+              to='/applied jobs'
+              aria-label='applied jobs'
+              title='applied jobs'
+              className={({ isActive }) => (isActive ? 'active' : 'font-medium tracking-wide text-gray-500  transition-colors duration-200')}
+            >
+             Applied Jobs
+            </NavLink>
+            <NavLink
+              to='/blog'
+              aria-label='blog'
+              title='blog'
+              className={({ isActive }) => (isActive ? 'active' : 'font-medium tracking-wide text-gray-500  transition-colors duration-200')}
+            >
+           Blog
+            </NavLink>
    
        </div>
       <div>
         <button className='bg-[#66cccc] rounded px-6 py-4 text-xl'>Appeal Now</button>
     </div>
-    </nav>
+      </nav>
+
+   
   );
 };
 
